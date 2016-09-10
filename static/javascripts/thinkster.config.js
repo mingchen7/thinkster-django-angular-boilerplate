b1885617da897e97bfa0d4a7e250bfe8fdd214f7
mingchen7 <mingchen7@email.arizona.edu>
2016-09-10 11:00:24 -0700

    finished logging users in

---
+++ static/javascripts/thinkster.config.js
@@ -0,0 +1,20 @@
+(function() {
+	'use strict';
+
+	angular
+	  .module('thinkster.config')
+	  .config(config);
+
+	config.$inject = ['$locationProvider'];
+
+	/**
+	* @name config
+	* @desc Enable HTML routing
+	*/
+
+	function config($locationProvider) {
+		$locationProvider.html5Mode(true);
+		$locationProvider.hashPrefix('!');
+	}
+
+})();
--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,9 +1,20 @@
-from django.conf.urls import patterns, url
+from django.conf import settings
+from django.conf.urls import include, patterns, url
+from rest_framework_nested import routers
+from authentication.views import AccountViewSet
+from authentication.views import LoginView
 
 from thinkster_django_angular_boilerplate.views import IndexView
 
+
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+
 urlpatterns = patterns(
     '',
-
+    # .. URLs
+    url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
     url('^.*$', IndexView.as_view(), name='index'),
 )
